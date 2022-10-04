071f08a3c7c4bbe9b2bd6e280a7b85f99112dbc5
Max <41009925+MAKblC@users.noreply.github.com>
2022-10-04 13:38:19 +0300

    Update test_greenhouse.ino

--- test_greenhouse.ino
+++ test_greenhouse.ino
@@ -33,23 +33,50 @@ BH1750FVI LightSensor_1;      // BH1750
 #include <Adafruit_BME280.h>  // BME280                         
 Adafruit_BME280 bme280;       //
 
-/* если у вас в комплектации вместо датчика УФ MGS-UV60 есть датчик MGS-GUVA - используйте эту библиотеку и код: https://github.com/MAKblC/Codes/tree/master/MGS-GUVA
-если MGS-CO30: https://github.com/MAKblC/Codes/tree/master/MGS-CO30
-*/
-#include <VEML6075.h>         // добавляем библиотеку датчика ультрафиолета // adding Ultraviolet sensor library        
-VEML6075 veml6075;            // VEML6075
+#include "MCP3221.h"
+#include "SparkFun_SGP30_Arduino_Library.h"
+#include <VEML6075.h>
 
+// Выберите плату расширения вашей сборки (ненужные занесите в комментарии)
+#define MGB_D1015 1
+//#define MGB_P8 1
+
+// Выберите датчик вашей сборки (ненужные занесите в комментарии)
+//#define MGS_GUVA 1
+#define MGS_CO30 1
+//#define MGS_UV60 1
+
+#ifdef MGS_CO30
+SGP30 mySensor;
+#endif
+#ifdef MGS_GUVA
+const byte DEV_ADDR = 0x4F;  // 0x5С , 0x4D (также попробуйте просканировать адрес: https://github.com/MAKblC/Codes/tree/master/I2C%20scanner)
+MCP3221 mcp3221(DEV_ADDR);
+#endif
+#ifdef MGS_UV60
+VEML6075 veml6075;
+#endif
+
+#ifdef MGB_D1015
 Adafruit_ADS1015 ads(0x48);
 const float air_value    = 83900.0;
 const float water_value  = 45000.0;
 const float moisture_0   = 0.0;
 const float moisture_100 = 100.0;  // настройка АЦП на плате расширения I2C MGB-D10 // I2C MGB-D10 ADC configuration
-
+#endif
+#ifdef MGB_P8
+#define SOIL_MOISTURE    34 // A6
+#define SOIL_TEMPERATURE 35 // A7
+const float air_value    = 1587.0;
+const float water_value  = 800.0;
+const float moisture_0   = 0.0;
+const float moisture_100 = 100.0;
+#endif
 //////////////////////////////////////////НАСТРОЙКИ/CONFIGURATION/////////////////////////////////////////////////////////////////
 void setup()
 {
   myservo.attach(19);             // пин сервомотора // servo pin
-
+  Wire.begin();
   pinMode( pump, OUTPUT );
   pinMode( wind, OUTPUT );       // настройка пинов насоса и вентилятора на выход // pump and cooler pins configured on output mode
   digitalWrite(pump, LOW);       // устанавливаем насос и вентилятор изначально выключенными // turn cooler and pump off
@@ -65,30 +92,59 @@ void setup()
 
   bool bme_status = bme280.begin();
   if (!bme_status)
-      Serial.println("Could not find a valid BME280 sensor, check wiring!");  // проверка  датчика температуры, влажности и давления // checking the temp hum bar sensor
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");  // проверка  датчика температуры, влажности и давления // checking the temp hum bar sensor
 
-  /* если у вас в комплектации вместо датчика УФ MGS-UV60 есть датчик MGS-GUVA - используйте эту библиотеку и код: https://github.com/MAKblC/Codes/tree/master/MGS-GUVA
-  если MGS-CO30: https://github.com/MAKblC/Codes/tree/master/MGS-CO30
-  */
+#ifdef MGS_UV60
   if (!veml6075.begin())
-    Serial.println("VEML6075 not found!");   // проверка работы датчика ультрафиолета  // checking the UV sensor
+    Serial.println("VEML6075 not found!");
+#endif
+#ifdef MGS_GUVA
+  mcp3221.setVinput(VOLTAGE_INPUT_5V);
+#endif
+#ifdef MGS_CO30
+  if (mySensor.begin() == false) {
+    Serial.println("No SGP30 Detected. Check connections.");
+    while (1);
+  }
+  mySensor.initAirQuality();
+#endif
 
+#ifdef MGB_D1015
   ads.setGain(GAIN_TWOTHIRDS);
   ads.begin();    // включем АЦП // turn the ADC on
+#endif
 }
 
 void loop()
 {
-  /* если у вас в комплектации вместо датчика УФ MGS-UV60 есть датчик MGS-GUVA - используйте эту библиотеку и код: https://github.com/MAKblC/Codes/tree/master/MGS-GUVA
-  если MGS-CO30: https://github.com/MAKblC/Codes/tree/master/MGS-CO30
-  */
+
+#ifdef MGS_UV60
   veml6075.poll();
   float uva = veml6075.getUVA();
   float uvb = veml6075.getUVB();
   float uv_index = veml6075.getUVIndex();
-  Serial.println("UV-А = " + String(uva, 1) + " μW/cm2");
-  Serial.println("UV-B = " + String(uvb, 1) + " μW/cm2");
-  Serial.println("UV-index = " + String(uv_index, 1));
+  Serial.println("UVA " + String(uva, 1));
+  Serial.println("UVB " + String(uvb, 1));
+  Serial.println("UVI " + String(uv_index, 1));
+#endif
+#ifdef MGS_GUVA
+  float sensorVoltage;
+  float sensorValue;
+  float UV_index;
+  sensorValue = mcp3221.getVoltage();
+  sensorVoltage = 1000 * (sensorValue / 4096 * 5.0); // напряжение на АЦП
+  UV_index = 370 * sensorVoltage / 200000; // Индекс УФ (эмпирическое измерение)
+  Serial.println("Выходное напряжение = " + String(sensorVoltage, 1) + "мВ");
+  Serial.println("UV index = " + String(UV_index, 1));
+#endif
+#ifdef MGS_CO30
+  mySensor.measureAirQuality();
+  Serial.print("CO2: ");
+  Serial.print(mySensor.CO2);
+  Serial.print(" ppm\tTVOC: ");
+  Serial.print(mySensor.TVOC);
+  Serial.println(" ppb");
+#endif
   delay(1512);
   float t = bme280.readTemperature();
   float h = bme280.readHumidity();
@@ -100,12 +156,22 @@ void loop()
   float l = LightSensor_1.getAmbientLight();
   Serial.println("light intensity = " + String(l, 1) + " lx");
   delay(1512);
+#ifdef MGB_D1015
   float adc0 = (float)ads.readADC_SingleEnded(0) * 6.144 * 16;
   float adc1 = (float)ads.readADC_SingleEnded(1) * 6.144 * 16;
   float t1 = (adc1 / 1000); //1023.0 * 5.0) - 0.5) * 100.0;
   float h1 = map(adc0, air_value, water_value, moisture_0, moisture_100);
   Serial.println("soil temperature = " + String(t1, 1) + " °C");
   Serial.println("soil humidity = " + String(h1, 1) + " %");
+#endif
+#ifdef MGB_P8
+  float adc0 = analogRead(SOIL_MOISTURE);
+  float adc1 = analogRead(SOIL_TEMPERATURE);
+  float t1 = ((adc1 / 4095.0 * 5.0) - 0.3) * 100.0; // АЦП разрядность (12) = 4095
+  float h1 = map(adc0, air_value, water_value, moisture_0, moisture_100);
+  Serial.println("soil temperature = " + String(t1, 1) + " °C");
+  Serial.println("soil humidity = " + String(h1, 1) + " %");
+#endif
   delay(1512);
   myservo.write(10);
   Serial.println("servo position = 10°");
